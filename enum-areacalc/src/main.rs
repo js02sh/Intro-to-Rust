bf8926f9015d5b694fabde1d1ea3f934b010fcd2
Jisang Yun <journeyofbabo@gmail.com>
2023-12-12 15:55:11 +0900

    print format

--- enum-areacalc/src/main.rs
+++ enum-areacalc/src/main.rs
@@ -9,7 +9,11 @@ enum Shape {
 impl Shape {
     fn area(&self) -> f64 {
         match *self {
-            Shape::Rectangle { width, height } => (width * height) as f64,
+            Shape::Rectangle { width, height } => {
+                let area = width * height;
+                println!("{}", format_args!("Width: {}, Height: {}, Area: {}", width, height, area));
+                area as f64
+            },
             Shape::Square(ref s) => (s * s) as f64,
             Shape::Circle(ref r) => 3.14 * (r * r),
         }
